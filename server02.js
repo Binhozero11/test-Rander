83839287aaa4cf8f8ac572c8b741458565a968d4
Binhozero11 <123664808+Binhozero11@users.noreply.github.com>
2023-12-02 09:36:57 -0300

    Update server02.js

--- server02.js
+++ server02.js
@@ -4,12 +4,28 @@ const app = express()
 
 app.use(express.json())
 
+const testHtml = `
+    <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando</title>
+</head>
+<body>
+    <h1>Fábio de Jesus Xavier</h1>
+    <p>Técnico em innformática</p>
+</body>
+</html>
+`
+
 app.get("/", function (req, res) {
-    res.status(200).json({
-        nome: "Bruno Túlio",
-        version: "1.0.0",
-        curso: "Info"
-    })
+ //   res.status(200).json({
+ //       nome: "Bruno Túlio",
+ //       version: "1.0.0",
+ //       curso: "Info"
+//  })
+    res.status(200).json(testHtml)
 })
 
 const port = process.env.PORT || 3333
